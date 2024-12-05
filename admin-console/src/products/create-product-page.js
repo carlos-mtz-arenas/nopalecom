a8ad6e139cd75f5fc6f016dd6793e82f481dc362
Carlos Martinez <mrcaleche@gmail.com>
2024-12-04 20:08:27 -0600

    Redirect back to listing products after creation

--- admin-console/src/products/create-product-page.js
+++ admin-console/src/products/create-product-page.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'lit';
 import { getMessage } from '../core/lang/get-message';
 import { createProduct } from './handlers/create-product';
+import { Router } from '@vaadin/router';
 
 
 export class CreateProductPage extends LitElement {
@@ -42,7 +43,7 @@ export class CreateProductPage extends LitElement {
       const dataValues = Object.fromEntries(formData);
 
       const uuid = await createProduct(dataValues);
-      console.log('registered', uuid);
+      Router.go('/products');
     } catch (err) {
       console.error('Error while creating a new product', err);
       // TODO handle the error
